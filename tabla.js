93066c13c06b610370e1c1e21ea2af2b426f9e9f
ÁLVARO LLAMAS HUERTA <alvaro.llahue@educa.jcyl.es>
2024-11-11 12:10:14 +0100

    11/11/2024

--- tabla.js
+++ tabla.js
@@ -3,10 +3,10 @@
 // Creamos una matriz en la que almacenamos los datos de los empleados
 // Nombre, Apellido, Año Nacimiento, Sueldo
 let empleados = [
-    {nombre: "Gabriel", apellido: "Herranz", nacimiento: 1954, sueldo: 30000 },
+    {nombre: "Gabriel", apellido: "Herranz", nacimiento: 1975, sueldo: 30000 },
     {nombre: "Sandra", apellido: "Lopez", nacimiento: 1984, sueldo: 55000},
-    {nombre: "Juan", apellido: "Gonzalez", nacimiento: 1990, sueldo: 45000},
-    {nombre: "Fernando", apellido: "Jimenez", nacimiento: 2000, sueldo: 25000},
+    {nombre: "Juan", apellido: "Gonzalez", nacimiento: 2002, sueldo: 45000},
+    {nombre: "Fernando", apellido: "Jimenez", nacimiento: 1900, sueldo: 25000},
 ];
 
 // Filtramos por sueldo > 40000
@@ -14,7 +14,7 @@ let empleadosSueldosAltos = empleados.filter(empleado => {
     return empleado.sueldo > 40000;
 });
 
-// Ordenamos los empleados
+// Ordenamos los empleados por año de nacimiento
 let empleadosPorOrdenDeNacimiento = empleados.sort((a, b) => {
-    return empleados.nacimiento(a - b);
+    return a.nacimiento - b.nacimiento;
 });
